0e5ccd212b9f5a35065835c0dd94c655a2a7e73b
Duc Pham <ducpham@C02V5109HV2H.local>
2018-08-17 23:52:58 +0700

    Update Context

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,30 +1,65 @@
 import React from "react";
 import { Button, Input } from "antd";
+import styled from "styled-components";
 
 const { Provider, Consumer } = React.createContext();
 
+const Container = styled.div`
+  border: 1px solid black;
+  padding: 30px;
+  margin: 10px;
+`;
+
 const Child3 = () => (
-  <Consumer>
-    {color => <Button style={{ background: color }}>Button With Theme</Button>}
-  </Consumer>
+  <Container>
+    Children 3
+    <Consumer>
+      {color => (
+        <Button style={{ background: color }}>Button With Theme</Button>
+      )}
+    </Consumer>
+  </Container>
 );
 
-const Child2 = () => <Child3 />;
+const Child2 = () => (
+  <Container>
+    Children 2<Child3 />
+  </Container>
+);
 
-const Child1 = () => <Child2 />;
+const Child1 = () => (
+  <Container>
+    Children 1<Child2 />
+  </Container>
+);
+
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 class ThemeProvider extends React.Component {
   state = { color: "red" };
 
-  handleChangeColor = event => {
-    this.setState({ color: event.target.value });
+  handleClick = event => {
+    const color = getRandomColor();
+    this.setState({ color });
   };
 
   render() {
+    const { color } = this.state;
+
     return (
-      <Provider value={this.state.color}>
-        <Input type="color" onChange={this.handleChangeColor} />
-        <Child1 />
+      <Provider value={color}>
+        <Container>
+          <Button onClick={this.handleClick}>{`Random Color ${color}`}</Button>
+          Parent
+          <Child1 />
+        </Container>
       </Provider>
     );
   }
